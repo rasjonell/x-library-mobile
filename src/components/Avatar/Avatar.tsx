1ce90d8da2ffd6d4dee05ef1726652e70f34e48b
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-08-16 19:00:23 +0400

    Make Avatar actionable

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
-import { Avatar as NBAvatar } from 'native-base';
+import { Avatar as NBAvatar, Icon } from 'native-base';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import { AVATAR_API_KEY } from '@env';
+import { Alert } from 'react-native';
 
 interface AvatarProps {
   seed: string;
+  action?: {
+    title: string;
+    message: string;
+    iconName: string;
+    onPress: () => void;
+  };
 }
 
-const Avatar = ({ seed }: AvatarProps) => {
+const Avatar = ({ seed, action }: AvatarProps) => {
   const uri = `https://api.multiavatar.com/${seed}.png?apikey=${AVATAR_API_KEY}`;
 
-  return <NBAvatar size="2xl" mt={5} bg="indigo.500" source={{ uri }} />;
+  const handleActionClick = () => {
+    if (!action) {
+      return;
+    }
+
+    Alert.alert(action.title, action.message, [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      { text: action.title, onPress: action.onPress },
+    ]);
+  };
+
+  return (
+    <NBAvatar size="2xl" mt={5} bg="indigo.500" source={{ uri }}>
+      {action ? (
+        <NBAvatar.Badge
+          bg="primary.500"
+          alignItems="center"
+          justifyContent="center">
+          <Icon
+            color="muted.100"
+            onPress={handleActionClick}
+            as={<AntDesign name={action.iconName} />}
+          />
+        </NBAvatar.Badge>
+      ) : null}
+    </NBAvatar>
+  );
 };
 
 export default Avatar;
