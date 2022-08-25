de53a7ba02284332627e68f5b5e5a7d7963f6df6
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-08-25 22:24:53 +0400

    Create `useSearch` query handler

--- src/api/library.ts
+++ src/api/library.ts
@@ -1,4 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+
 import { useAxios } from '../context/Axios';
 
 export const useUserReviews = () => {
@@ -8,4 +10,33 @@ export const useUserReviews = () => {
     const response = await AuthAPI.get('/users/reviews');
     return response.data.data;
   });
+};
+
+export const useSearch = () => {
+  const { PublicAPI } = useAxios();
+  const queryClient = useQueryClient();
+  const [latestQuery, setLatestQuery] = useState<string>('');
+
+  return async (query: string): Promise<Models.OpenLibrary.Doc[] | null> => {
+    if (latestQuery && query === latestQuery) {
+      return (
+        queryClient.getQueryData<Models.OpenLibrary.Doc[]>(['Search', query]) ||
+        null
+      );
+    }
+
+    if (!query) {
+      return null;
+    }
+
+    setLatestQuery(query);
+
+    const url = `https://openlibrary.org/search.json?q=${query}`;
+    const response = await PublicAPI.get(url);
+    const validDocs = (
+      response.data.docs as Models.OpenLibrary.DocResponse[]
+    ).filter(doc => doc.publisher && doc.author_name && doc.subject);
+
+    return validDocs as Models.OpenLibrary.Doc[];
+  };
 };
---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { useSearch as useSearchQuery } from '../api/library';
+import useDebounce from './useDebounce';
+
+type UseSearchReturn = [
+  string,
+  () => void,
+  () => void,
+  (newQuery: string) => void,
+  {
+    isError: boolean;
+    isLoading: boolean;
+    data: Models.OpenLibrary.Doc[] | null | undefined;
+  },
+];
+
+export default function useSearch(): UseSearchReturn {
+  const searchBooks = useSearchQuery();
+  const [query, setQuery] = useState<string>('');
+  const debouncedQuery = useDebounce(query, 600);
+  const { data, isLoading, isError, refetch } = useQuery(
+    ['Search', debouncedQuery],
+    () => searchBooks(debouncedQuery),
+  );
+
+  const onSubmit = () => {
+    refetch();
+  };
+
+  const clearQuery = () => {
+    setQuery('');
+  };
+
+  const onQueryChange = (newQuery: string) => {
+    setQuery(newQuery);
+  };
+
+  return [
+    query,
+    onSubmit,
+    clearQuery,
+    onQueryChange,
+    { data, isLoading, isError },
+  ];
+}
